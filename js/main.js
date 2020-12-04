be6114c23e44df12e6337ab5cbff03fe13583d86
ballardinimarta <marta.ballardini@gmail.com>
2020-12-04 16:26:31 +0100

    Add hero and make constructor

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-//insert JS here
+class Product {
+    constructor(name, category, price, image, id, description) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.image = image;
+        this.id = id;
+        this.description = description;
+    }
+}
+let id = 0;
+let products = [];
+$(function () {
+
+    let kettleSaltAndVinegar = new Product("Kettle Brand Potato Chips, Sea Salt and Vinegar Kettle Chips", "Chips", 39, "assets/kettleSaltAndVinegar.jpeg", id++, "Ingredients: Potatoes, Safflower And/Or Sunflower And/Or Canola Oil, Vinegar Powder (Maltodextrin, White Distilled Vinegar), Sea Salt, Maltodextrin, Citric Acid." );
+})
